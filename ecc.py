baaa949688afd39d75c46c58fdac2e50b11e5349
Alex Briskin <br.shurik@gmail.com>
2021-02-24 02:01:55 +0200

    Dirty implementation

--- ecc.py
+++ ecc.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """alex_ecc_experiment.ecc
 
@@ -63,7 +62,7 @@ def calculate_slope(x1, y1, x2, y2, prime, a):
     )
 
 
-def calculate_sum(x1, y1, x2, y2, prime, a):
+def point_add(x1, y1, x2, y2, prime, a):
     """ Semantics of point at infinity is y coordinate is None"""
     if (x1, y1) == (x2, -1 * y2 % prime):
         return x1, None
@@ -79,50 +78,114 @@ def calculate_sum(x1, y1, x2, y2, prime, a):
 
 def point_multiply(x1, y1, multiplicant, prime, a):
     assert multiplicant >= 1
-    _calculate_sum = partial(calculate_sum, prime=prime, a=a)
     x2, y2 = x1, y1
     # Skip first '1' digit and start double and add sequence
     for digit in bin(multiplicant)[3:]:
-        x2, y2 = _calculate_sum(x2, y2, x2, y2)
+        x2, y2 = point_add(x2, y2, x2, y2, prime, a)
         if digit == "1":
-            x2, y2 = _calculate_sum(x1, y1, x2, y2)
+            x2, y2 = point_add(x1, y1, x2, y2, prime, a)
 
     return x2, y2
 
 
 domain_param = namedtuple("domain_param", "prime a b G_x G_y n")
 domain_params = {
-    "p-192": domain_param(
+    "P-192": domain_param(
         prime=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFFFFFFFFFFFF,
         b=0x64210519E59C80E70FA7E9AB72243049FEB8DEECC146B9B1,
         a=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFFFFFFFFFFFC,
         G_x=0x188DA80EB03090F67CBF20EB43A18800F4FF0AFD82FF1012,
         G_y=0x07192B95FFC8DA78631011ED6B24CDD573F977A11E794811,
         n=0xFFFFFFFFFFFFFFFFFFFFFFFF99DEF836146BC9B1B4D22831,
-    )
+    ),
+    "P-224": domain_param(
+        prime=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF000000000000000000000001,
+        a=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFE,
+        b=0xB4050A850C04B3ABF54132565044B0B7D7BFD8BA270B39432355FFB4,
+        G_x=0xB70E0CBD6BB4BF7F321390B94A03C1D356C21122343280D6115C1D21,
+        G_y=0xBD376388B5F723FB4C22DFE6CD4375A05A07476444D5819985007E34,
+        n=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFF16A2E0B8F03E13DD29455C5C2A3D,
+    ),
+    "P-256": domain_param(
+        prime=0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF,
+        a=0xFFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC,
+        b=0x5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B,
+        G_x=0x6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296,
+        G_y=0x4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5,
+        n=0xFFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551,
+    ),
+    "P-384": domain_param(
+        prime=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFFFF0000000000000000FFFFFFFF,
+        a=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFFFF0000000000000000FFFFFFFC,
+        b=0xB3312FA7E23EE7E4988E056BE3F82D19181D9C6EFE8141120314088F5013875AC656398D8A2ED19D2A85C8EDD3EC2AEF,
+        G_x=0xAA87CA22BE8B05378EB1C71EF320AD746E1D3B628BA79B9859F741E082542A385502F25DBF55296C3A545E3872760AB7,
+        G_y=0x3617DE4A96262C6F5D9E98BF9292DC29F8F41DBD289A147CE9DA3113B5F0B8C00A60B1CE1D7E819D7A431D7C90EA0E5F,
+        n=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC7634D81F4372DDF581A0DB248B0A77AECEC196ACCC52973,
+    ),
+    "P-521": domain_param(
+        prime=2 ** 521 - 1,
+        a=2 ** 521 - 4,
+        b=0x0051953EB9618E1C9A1F929A21A0B68540EEA2DA725B99B315F3B8B489918EF109E156193951EC7E937B1652C0BD3BB1BF073573DF883D2C34F1EF451FD46B503F00,
+        G_x=0xC6858E06B70404E9CD9E3ECB662395B4429C648139053FB521F828AF606B4D3DBAA14B5E77EFE75928FE1DC127A2FFA8DE3348B3C1856A429BF97E7E31C2E5BD66,
+        G_y=0x11839296A789A3BC0045C8A5FB42C7D1BD998F54449579B446817AFBD17273E662C97EE72995EF42640C550B9013FAD0761353C7086A272C24088BE94769FD16650,
+        n=0x01FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA51868783BF2F966B7FCC0148F709A5D03BB5C9B8899C47AEBB6FB71E91386409,
+    ),
 }
 
 
-if __name__ == "__main__":
-    # value = ecc_test_vector('/home/alex/Documents/Crypto/TestVectors/ECDSA/KeyPair.rsp')
+def test_key_gen_vectors():
     for curve_name, curve_description, vector in ecc_test_vector(
         "/home/alex/Documents/Crypto/TestVectors/ECDSA/KeyPair.rsp"
     ):
-        _, y_res = point_multiply(G_x, G_y, n, prime_192, a_192)
+        if curve_name not in domain_params:
+            continue
+        prime, a, _, G_x, G_y, n = domain_params[curve_name]
+        _, y_res = point_multiply(G_x, G_y, n, prime, a)
         assert y_res is None
         d, Qx, Qy = itemgetter("d", "Qx", "Qy")(vector)
-        calculated_point = point_multiply(G_x, G_y, d, prime_192, a_192)
+        calculated_point = point_multiply(G_x, G_y, d, prime, a)
         assert calculated_point == (
             Qx,
             Qy,
         ), f"{curve_name} - {calculated_point} != {(Qx, Qy)}"
-    # d = 0xE5CE89A34ADDDF25FF3BF1FFE6803F57D0220DE3118798EA
-    # Qx = 0x8ABF7B3CEB2B02438AF19543D3E5B1D573FA9AC60085840F
-    # Qy = 0xA87F80182DCD56A6A061F81F7DA393E7CFFD5E0738C6B245
-    # expected = point_multiply(G_x, G_y, d, prime_192, a_192)
-    # assert expected == (Qx, Qy)
 
-#     for multiplicant in range(1, 23):
-#         print(f"{multiplicant:-2d}: {point_multiply(5, 1, multiplicant, 17, 2)}")
-#         Q = Q + P
-#         print(Q)
+
+def test_sign_gen_vectors():
+    Msg = 0x699325D6FC8FBBB4981A6DED3C3A54AD2E4E3DB8A5669201912064C64E700C139248CDC19495DF081C3FC60245B9F25FC9E301B845B3D703A694986E4641AE3C7E5A19E6D6EDBF1D61E535F49A8FAD5F4AC26397CFEC682F161A5FCD32C5E780668B0181A91955157635536A22367308036E2070F544AD4FFF3D5122C76FAD5D
+    d = 0x16797B5C0C7ED5461E2FF1B88E6EAFA03C0F46BF072000DFC830D615
+    Qx = 0x605495756E6E88F1D07AE5F98787AF9B4DA8A641D1A9492A12174EAB
+    Qy = 0xF5CC733B17DECC806EF1DF861A42505D0AF9EF7C3DF3959B8DFC6669
+    k = 0xD9A5A7328117F48B4B8DD8C17DAE722E756B3FF64BD29A527137EEC0
+    R = 0x2FC2CFF8CDD4866B1D74E45B07D333AF46B7AF0888049D0FDBC7B0D6
+    S = 0x8D9CC4C8EA93E0FD9D6431B9A1FD99B88F281793396321B11DAC41EB
+    prime, a, _, G_x, G_y, n = domain_params["P-224"]
+    x_res, y_res = point_multiply(G_x, G_y, d, prime, a)
+    assert (x_res, y_res) == (Qx, Qy)
+    assert 1 <= k < n
+    r, _ = point_multiply(G_x, G_y, k, prime, a)
+    r = r % n
+    assert r
+    assert r == R
+    from hashlib import sha224
+
+    msg = Msg.to_bytes((Msg.bit_length() + 7) // 8, byteorder="big")
+    z = int.from_bytes(sha224(msg).digest(), byteorder="big")
+    s = (z + r * d) * mod_inverse(k, n) % n
+    assert s
+    assert s == S, f"{s:x} != {S:x}"
+    assert (r, s) == (R, S)
+    ################################################################################
+    w = mod_inverse(s, n)
+    u = z * w % n
+    v = r * w % n
+    x, y = point_add(
+        *point_multiply(G_x, G_y, u, prime, a),
+        *point_multiply(Qx, Qy, v, prime, a),
+        prime,
+        a,
+    )
+    assert r == x % n, f"{r:x} != {x % n:x}"
+
+
+if __name__ == "__main__":
+    pass
